c24d8b79d4ffcecad8a22460eb491fb0ce5b93b5
Ethan Hayon <ehayon@gmail.com>
2011-11-22 12:45:00 -0500

    added problem 9 in python

---
+++ problem9.py
@@ -0,0 +1,11 @@
+a,b,c = 1,2,3
+while(1):
+    if (a*a) + (b*b) == (c*c):
+        if (a + b + c) == 1000:
+            print a*b*c
+            break
+    a += 1
+    b += 1
+    c += 1
+        
+
